bd0dd152c873d3838363637faf41903a7919ed4e
Parth Sareen <parth0612@live.com>
2018-10-25 17:39:53 -0400

    Add files via upload

--- HashTsble/src/hash_table.c
+++ HashTsble/src/hash_table.c
@@ -52,8 +52,60 @@ static int ht_get_hash(const char* s, const int num_buckets, const int attempt) 
 	const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
 	return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
+
+//ht new sized
+static ht_hash_table* ht_new_sized(const int base_size) {
+	ht_hash_table* ht = xmalloc(sizeof(ht_hash_table));
+	
+	ht->base_size = base_size;
+	
+	ht->count = 0;
+	ht->items = xcalloc((size_t)ht-> size, sizeof(ht_item*));
+	
+}
+
+static void ht_resize(ht_hash_table* ht, const int base_size_{
+	if (base_size <HT_INITIAL_BASE_SIZE) {
+		return;
+	}
+	ht_has_table* new_ht = ht_new_sized(base_size);
+	for(int i = 0; i < ht->size; i++){
+		if(item != NULL && item != &HT_DELETED_ITEM) {
+			ht_insert(new_ht, item->key, item-> value);
+		}
+	}
+	ht->base_size = new_ht->base_size;
+	ht->count = new_ht->count;
+	
+	//To delete new_ht, we give it ht's size and items
+	const int tmp_size = ht->size;
+	ht->size = new_ht->size;
+	new_ht->size = tmp_size;
+	
+	ht_item** tmp_items = ht-> items;
+	ht->items = new_ht -> items;
+	
+	ht_del_hash_table(new_ht);
+}
+static void ht_resize_up(ht_hash_table* ht) {
+    const int new_size = ht->base_size * 2;
+    ht_resize(ht, new_size);
+}
+
+
+static void ht_resize_down(ht_hash_table* ht) {
+    const int new_size = ht->base_size / 2;
+    ht_resize(ht, new_size);
+}
+ht_hash_table* ht_new() {
+	return ht_new_sized(HT_INITIAL_BASE_SIZE);
+}
 //insert
 void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
+	const int load = ht->count *100/ ht->size;
+	if (load > 70) {
+		ht_resize_up(ht);
+	}
 	ht_item* item = ht_new_item(key, value);
 	int index = ht_get_hash(item->key, ht-> size, 0);
 	ht_item* cur_item = ht->items[index];
@@ -96,6 +148,10 @@ char* ht_search(ht_hash_table ht, const char* key) {
 static ht_item HT_DELETED_ITEM = {NULL, NULL};
 
 void ht_delete(ht_hash_table* ht, const char* key) {
+	const int laod = ht-> count*100 / ht->size;
+	if (load < 10) {
+		ht_resize_down(ht);
+	}
 	int index = ht->items[index];
 	int i = 1;
 	while (item != NULL) {
