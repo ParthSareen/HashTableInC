a9c7d40559527918c26966e3ab91908811bf82be
Parth Sareen <parth0612@live.com>
2018-10-24 22:43:58 -0400

    Add files via upload

--- HashTsble/src/hash_table.c
+++ HashTsble/src/hash_table.c
@@ -53,12 +53,21 @@ static int ht_get_hash(const char* s, const int num_buckets, const int attempt) 
 	return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
 //insert
-void ht_insert(ht_hash_table* ht, const char* value) {
+void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
 	ht_item* item = ht_new_item(key, value);
 	int index = ht_get_hash(item->key, ht-> size, 0);
 	ht_item* cur_item = ht->items[index];
 	int i = 1;
-	while(cur_item != NULL) {
+	while (cur_item != NULL) {
+		if(cur_item != &HT_DELETED_ITEM) {
+			if(strcmp(cur_item -> key, key) ==0) {
+				ht_del_item(cur_item);
+				ht->items[index] = item;
+				return;
+			}
+		}
+	}
+	while(cur_item != NULL && cur_item != &HT_DELETED_ITEM) {
 	index = ht_get_hash(item->key, ht->size, i);
 	cur_item = ht->items[index];
 	i++;
@@ -72,8 +81,10 @@ char* ht_search(ht_hash_table ht, const char* key) {
 	ht_item* item  = ht->items[index];
 	int i = 1;
 	while (item != NULL) {
-		if(strcmp(item->key, key) == 0) {
-			return item-> value;
+		if (item != &HT_DELETED_ITEM){
+			if(strcmp(item->key, key) == 0) {
+				return item-> value;
+			}
 		}
 		index = ht_get_hash(key, ht->size, i);
 		item = ht-> items[index];
@@ -100,6 +111,8 @@ void ht_delete(ht_hash_table* ht, const char* key) {
 	}
 	ht->count--;
 }
+
+
 
 	//main.c
 int main() {
